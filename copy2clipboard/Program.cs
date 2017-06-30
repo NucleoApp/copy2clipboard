a4c68db182674d4d5efc08127c54b66a3271b37a
Dmytro <sharpfuryz@gmail.com>
2017-06-30 15:44:33 +0300

    Initial commit

--- copy2clipboard/Program.cs
+++ copy2clipboard/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Specialized;
 
 namespace copy2clipboard
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            string filepath;
+            if(args.Length > 0)
+            {
+                filepath = args[0];
+                if (filepath.ToLower().Contains(".svg"))
+                {
+                    System.Console.WriteLine("SVG is being copied to clipboard");
+                    copyAsSVG(filepath);
+                }
+                System.Console.WriteLine(args[0]);
+            }
+            else
+            {
+                System.Console.WriteLine("No arguments specified, closing");
+            }
+        }
+
+        private static void copyAsSVG(string filepath)
+        {
+            //StringCollection paths = new StringCollection
+            //{
+            //    filepath
+            //};
+            //Clipboard.SetFileDropList(paths);
+            string svg = File.ReadAllText(filepath);
+            byte[] bytes = Encoding.UTF8.GetBytes(svg);
+            MemoryStream stream = new MemoryStream(bytes);
+            //DataObject dataObj = new DataObject();
+            //dataObj.SetData(stream);
+            Clipboard.SetData("image/svg+xml", stream);
         }
     }
 }
---
+++ copy2clipboard/test_svg.bat
@@ -0,0 +1,2 @@
+C:\workspace\copy2clipboard\copy2clipboard\bin\Debug\copy2clipboard.exe C:\workspace\copy2clipboard\copy2clipboard\test.svg
+pause
